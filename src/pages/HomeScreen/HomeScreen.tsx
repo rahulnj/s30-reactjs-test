9cedf34d7b78b4dbc62e69d0c36b485c344acaf6
rahulnj~ <rahuljayaraman8@gmail.com>
2022-07-03 16:04:37 +0530

    changed the thumbnail to large.

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -31,15 +31,17 @@ const HomeScreen = () => {
                 <div className='homeScreen_parent_wrapper'>
                     <div className='homeScreen_parent_wrapper_img'>
                         <img src={loading ? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png' :
-                            data?.picture?.thumbnail} alt="" />
+                            data?.picture?.large} alt="user" />
                     </div>
                     <div className='homeScreen_parent_wrapper_userDetails'>
                         <h4>
                             {data?.name?.first} {data?.name?.last}
                         </h4>
                         <p>{data?.email}</p>
+
                     </div>
                     <Button
+                        loading={loading}
                         btnText={'Reload'}
                         iconUrl={'https://htmlacademy.ru/assets/icons/reload-6x-white.png'}
                         onClick={() => hanldeFetchUserDetails()}
